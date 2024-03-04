97a58b1e1df50f6a15a64e493cf24c29e3693c2c
TobyZedomi <tobyzedo4@gmail.com>
2024-03-04 20:13:36 +0000

    Test if element to be removed is null

--- Part2_LinkedLists/src/test/java/utils/LinkedListExtraTests.java
+++ Part2_LinkedLists/src/test/java/utils/LinkedListExtraTests.java
@@ -15,8 +15,27 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class LinkedListExtraTests {
 
     public LinkedListExtraTests() {
+
     }
 
+
+    /**
+     * Test if element to be removed is null
+     */
+    @Test
+    public void testRemoveIfNull() {
+
+        System.out.println("Test if element to be removed is null");
+        Song s = new Song(null,null);
+        LinkedList instance = new LinkedList();
+        instance.add(new Song("Prince", "Sign O The Times"));
+        instance.add(new Song("Jimi Hendrix", "Axis Bold As Love"));
+        instance.add(new Song("Carti", "WLR"));
+
+        boolean expResult = false;
+        boolean result = instance.remove(s);
+        assertEquals(expResult, result);
+    }
 
 
 }
