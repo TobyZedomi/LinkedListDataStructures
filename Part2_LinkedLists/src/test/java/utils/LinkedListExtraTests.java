260887b5eb750fe91b1ac72b21ef9355c5516436
TobyZedomi <tobyzedo4@gmail.com>
2024-03-04 20:12:51 +0000

    LinkedListExtraTest

---
+++ Part2_LinkedLists/src/test/java/utils/LinkedListExtraTests.java
@@ -0,0 +1,22 @@
+package utils;
+
+import business.Song;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ *
+ * @author michelle
+ */
+public class LinkedListExtraTests {
+
+    public LinkedListExtraTests() {
+    }
+
+
+
+}
